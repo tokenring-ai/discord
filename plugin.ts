cd4cbfc732fe3cd0f21cd3b85892488ae428e5e4
Mark Dierolf <mdierolf@cloudquote.io>
2025-12-18 13:53:39 -0800

    Extract and modularize plugin logic into `plugin.ts`

---
+++ plugin.ts
@@ -0,0 +1,18 @@
+import TokenRingApp from "@tokenring-ai/app";
+import {TokenRingPlugin} from "@tokenring-ai/app";
+import packageJSON from './package.json' with {type: 'json'};
+import DiscordService, {DiscordServiceConfigSchema} from "./DiscordService.ts";
+
+
+export default {
+  name: packageJSON.name,
+  version: packageJSON.version,
+  description: packageJSON.description,
+  install(app: TokenRingApp) {
+    const discordConfig = app.getConfigSlice("discord", DiscordServiceConfigSchema.optional());
+
+    if (discordConfig) {
+      app.addServices(new DiscordService(app, discordConfig));
+    }
+  },
+} as TokenRingPlugin;
