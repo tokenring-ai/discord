2fd44ba0d83445c0bc9d957ce21158d7bd12beeb
Mark Dierolf <mdierolf@financialcontent.com>
2026-08-06 15:20:37 -0700

    Update Discord command schema to rename save options to "global" and "workspace" with default "workspace"

--- commands/connect/discord.ts
+++ commands/connect/discord.ts
@@ -12,8 +12,8 @@ const inputSchema = {
     save: {
       description: "Where to save the Discord account configuration",
       type: "enum",
-      values: ["user", "project"],
-      defaultValue: "user",
+      values: ["global", "workspace"],
+      defaultValue: "workspace",
     },
   },
   positionals: [
